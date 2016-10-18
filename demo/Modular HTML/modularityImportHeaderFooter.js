1610b4d489adc1d85bdf403d4a51e4843f36e439
Artorp <Artorp@users.noreply.github.com>
2016-10-18 17:27:35 +0200

    Initial files

---
+++ demo/Modular HTML/modularityImportHeaderFooter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Thomas on 2016-10-18.
+ * NOTE! Loading modular content with javascript is bad practice
+ * This is only done as a proof-of-concept
+ * A proper way to implement this is to use server-side rendering for the initial view
+ * Instead of using client-side rendering for the initial view
+ */
+
