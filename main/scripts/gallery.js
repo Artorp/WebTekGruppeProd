011667daea340d241cbc5d42bfbe3b21c2be87bd
Andreas Bergmo Johnsen <andreasbjohnsen@gmail.com>
2016-10-25 13:17:22 +0200

    litt javascript

--- main/scripts/gallery.js
+++ main/scripts/gallery.js
@@ -1,2 +1,16 @@
 /*Javascript for gallery.html (bildegalleri)                 
 */
+
+ function next()
+  {
+    var myImage= new Array(); 
+    myImage[0]="arkademaskiner.jpg";       
+    myImage[1]="cocktail_cab.jpg";
+    myImage[2]="pinball.jpg";
+    myImage[3]="kakerkaffe.jpg"; 
+	myImage[4]="Spaceinvaders.jpg.jpg";
+
+    ImageCnt++;
+
+
+  }
