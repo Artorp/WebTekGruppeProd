df3fd075b17e1e6681fd3edce6c33beda0fa2722
Tor Niklas <torniklas@outlook.com>
2016-10-28 13:06:49 +0200

    Compeltet js and other minor details

--- main/scripts/checkforEmptyFields.js
+++ main/scripts/checkforEmptyFields.js
@@ -1,26 +1,36 @@
-let name = document.getElementById("navn");
-let adress = document.getElementById("adresse");
+let name 	= document.getElementById("navn");
+let adress 	= document.getElementById("adresse");
+let email 	= document.getElementById("epost");
 let interests = document.getElementById("interesser");
-let exp = document.getElementById("erfaring");
-let whyW = document.getElementById("hvorforJobb");
-let whyB = document.getElementById("hvorforBedre");
+let exp 	= document.getElementById("erfaring");
+let whyW 	= document.getElementById("hvorforJobb");
+let whyB 	= document.getElementById("hvorforBedre");
 
 
 function mcheck(){
-	console.log("ME")
-	a = name.innerHTML 		!= "";
-	b = adress.innerHTML 	!= "";
-	c = interests.innerHTML	!= "";
-	d = exp.innerHTML		!= "";
-	e = whyW.innerHTML		!= "";
-	f = whyB.innerHTML		!= "";
-	g = (a && b);
-	if((a) && (b) && (c) && (d) && (e) && (f)){
-		console.log("YEY");
+	// console.log("ME")
+	a = name.value		!= "";
+	b = adress.value 	!= "";
+	c = email.value		!= "";
+	d = interests.value	!= "";
+	e = exp.value		!= "";
+	f = whyW.value		!= "";
+	g = whyB.value		!= "";
+	if((a)&&(b)&&(c)&&(d)&&(e)&&(f)&&(g)){
+		allFilled();
 	}
 	else{
 		console.log("NAY");
-	}
-}
-
-console.log("HI")
+	};
+};
+function allFilled(){
+	console.log("Information sent to server:");
+	console.log("Name: " + name.value);
+	console.log("Adress: " + adress.value);
+	console.log("Email: " + email.value);
+	console.log("Interests: " + interests.value);
+	console.log("Experience: " + exp.value);
+	console.log("Why work here: " + whyW.value);
+	console.log("Why better: " + whyB.value);
+	alert("Skjemaet er senndt inn")
+};
