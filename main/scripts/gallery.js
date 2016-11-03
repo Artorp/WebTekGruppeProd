5eccaef7d0bdc035a15ee227882e6e3af10d17d1
Andreas Bergmo Johnsen <andreasbjohnsen@gmail.com>
2016-11-03 05:35:48 +0100

    Gallery changes
    Still some leftover code that might have to be removed, in the end(such
    as the buttons)

--- main/scripts/gallery.js
+++ main/scripts/gallery.js
@@ -4,23 +4,105 @@ Filename: gallery.js*/
 
 /*Javascript for gallery.html (bildegalleri)
 
-Bare en start. Ikke funksjonell kode enda.
 */
+function getElementLeft(funk)
+{
+    var x = 0;
 
- function next()
-  {
-    var myImage= new Array();
-    myImage[0]="arkademaskiner.jpg";
-    myImage[1]="cocktail_cab.jpg";
-    myImage[2]="pinball.jpg";
-    myImage[3]="kakerkaffe.jpg";
-	myImage[4]="Spaceinvaders.jpg";
+   
+    x = funk.offsetLeft;
 
-    ImageCnt++;
+    
+    funk = funk.offsetParent;
+    
+
+    while(funk != null)
+    {
+        x = parseInt(x) + parseInt(funk.offsetLeft);
+        funk = funk.offsetParent;
+    }
+    return x;
+}
+
+function getElementTop(funk)
+{
+    var y = 0;
 
 
-  }
-  function pictureChange()
-  {
-  document.getElementById("myImage").src="img";
-  }
+    y = funk.offsetTop;
+
+
+    funk = funk.offsetParent;
+
+
+    while(funk != null)
+    {
+        y = parseInt(y) + parseInt(funk.offsetTop);
+        funk = funk.offsetParent;
+    }
+
+    return y;
+}
+
+function Large(obj)
+{
+    var bigimage=document.getElementById("bigimage");
+    bigimage.style.visibility='visible';
+    var img = document.createElement("img");
+
+    img.src=obj.src;
+    img.style.width='720px';
+    img.style.height='480px';
+
+    if(img.addEventListener){
+        img.addEventListener('mouseout',Out,false);
+    } else {
+        img.attachEvent('onmouseout',Out);
+    }
+
+    bigimage.innerHTML='';
+    bigimage.appendChild(img);
+    bigimage.style.left=(getElementLeft(obj)-0) +'px';
+    bigimage.style.top=(getElementTop(obj)-300) + 'px';
+}
+
+function Out()
+{
+    document.getElementById("bigimage").style.visibility='hidden';
+}
+
+var knappebilder = new Array(5);   // Not working at the moment. Might drop it.
+knappebilder[0] = "img/arkademaskiner.jpg";
+knappebilder[1] = "img/cocktail_cab.jpg";
+knappebilder[2] = "img/pinball.jpg";
+knappebilder[3] = "img/kakerkaffe.jpg";
+knappebilder[4] = "img/Spaceinvaders.jpg";
+
+
+var antallbilder = 4;
+var currentbilde= 1;
+
+function  backwards() {
+    var im=document.getElementById("bilde1");
+    if(curimg>0)
+    {
+        im.src = images[curimg-1];
+        curimg = curimg - 1;
+    }else{
+        alert("This is the first image");
+    }
+
+
+}
+
+function gofwd(){
+    var im=document.getElementById("image");
+    if(curimg < numimg){
+        im.src = images[curimg+1];
+        curimg = curimg + 1;
+    }else{
+        alert("This is the last image");
+    }
+
+
+}
