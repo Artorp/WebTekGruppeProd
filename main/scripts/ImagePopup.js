7b7e6f634491a426640fb3f5589b8ee6464a123c
Artorp <Artorp@users.noreply.github.com>
2016-11-09 23:08:19 +0100

    Refactor ImagePopup.js - support nonID

--- main/scripts/ImagePopup.js
+++ main/scripts/ImagePopup.js
@@ -1,65 +1,57 @@
 var closeModalTimeout = false;
 
 function closeModal() {
-	var theModal = document.getElementById('myModal');
-	if (theModal.style.display != "none" && closeModalTimeout) {
-		closeModalTimeout = false;
-		theModal.style.display = "none";
-	}
+    var theModal = document.getElementById('myModal');
+    if (theModal.style.display != "none" && closeModalTimeout) {
+        closeModalTimeout = false;
+        theModal.classList.add("modal-hidden");
+    }
 }
 
-addEvent(window, "click", closeModal);
+addEvent(window, "click", closeModal); // If user clicks while modal is open, close it
 
 function addModal() {
-	let node1 = document.createElement('div');
-	node1.setAttribute('id','myModal');
-	node1.setAttribute('class','modal');
-	
-	let node2 = document.createElement('span');
-	node2.setAttribute('class', 'close');
-	
-	let textnode = document.createTextNode('×');
-	node2.appendChild(textnode);
-	
-	let node3 = document.createElement('img');
-	node3.setAttribute('class','modal-content');
-	node3.setAttribute('id', 'img01');
-	
-	node1.appendChild(node2);
-	node1.appendChild(node3);
-	
-	document.body.appendChild(node1);
-	
-	
-	return node1;
+    let node1 = document.createElement('div');
+    node1.setAttribute('id', 'myModal');
+    node1.classList.add("modal");
+    node1.classList.add("modal-hidden");
+
+    let node2 = document.createElement('span');
+    node2.setAttribute('class', 'close');
+
+    let textnode = document.createTextNode('×');
+    node2.appendChild(textnode);
+
+    let node3 = document.createElement('img');
+    node3.setAttribute('class', 'modal-content');
+    node3.setAttribute('id', 'modal-img-element');
+
+    node1.appendChild(node2);
+    node1.appendChild(node3);
+
+    document.body.appendChild(node1);
+
+
+    return node1;
 }
 
 var ModalEl = addModal();
 
 
-classname = document.getElementsByClassName('popup-picture');
+var modal_images_HTMLCol = document.getElementsByClassName('popup-picture');
 
 
-
-for (let i = 0; i < classname.length; i++) {
-	classname[i].addEventListener('click', getID)
+for (let i = 0; i < modal_images_HTMLCol.length; i++) {
+    modal_images_HTMLCol[i].addEventListener('click', modalPopup);
 }
 
 
-function modalPopup (id){
-	// Get the modal
-	var modal = document.getElementById('myModal');
+function modalPopup(event) {
+    // Get the image and insert it inside the modal
+    ModalEl.classList.remove("modal-hidden");
+    document.getElementById("modal-img-element").src = event.target.src;
 
-	// Get the image and insert it inside the modal
-	var img = document.getElementById(id);
-	var modalImg = document.getElementById("img01");
-	modal.style.display = "block";
-	modalImg.src = document.getElementById(id).src;
-
-}
-
-function getID () {
-	 id = this.id;
-	 modalPopup(id);
-	setTimeout(function () { closeModalTimeout = true; },100);
+    setTimeout(function () {
+        closeModalTimeout = true;
+    }, 100);
 }
