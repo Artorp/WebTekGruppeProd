cd61d12bf56a08f335763c08ca79e9d5f568e063
Andreas Bergmo Johnsen <andreasbjohnsen@gmail.com>
2016-11-11 13:45:51 +0100

    mer

--- main/scripts/gallery.js
+++ main/scripts/gallery.js
@@ -1,33 +1,25 @@
-/* Author: Andreas
- When: November 2016
- Filename: gallery.js*/
 /*Author: Andreas
  When: November 2016
  Filename: gallery.js
  Purpose: Javascript for "Bildegalleri".
  */
 
-
-
-function Large(obj) {
+function Large(obj) { // Purpose: Create a big image that serve as the currently viewed picture.
     var bigimage = document.getElementById("bigimage");
     var img = document.createElement("img");
     img.src = obj.src;
 
-    // img.style.maxHeight = '480px' Decided to style this with the CSS (gallery.css) instead;
-    //max-width: 100% is also styled in CSS;
+
 
     bigimage.innerHTML = '';
     bigimage.appendChild(img);
-    //bigimage.style.left = (getElementLeft(obj) - 0) + 'px'; (See CSS)
-    //bigimage.style.top = (getElementTop(obj) - 300) + 'px'; (See CSS)
+
 }
 
 function Out() {
     document.getElementById("bigimage").style.visibility = 'hidden';
 }
 
-// Set up default picture ("0" is the first picture)
-console.log(document.getElementsByClassName("picelement")[0].childNodes[1]);
+// Set up default picture ("0" is the first picture. Could also use number between 1-4)
 Large(document.getElementsByClassName("picelement")[0].childNodes[1]);
 
