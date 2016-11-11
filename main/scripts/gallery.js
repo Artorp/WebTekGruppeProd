c79e370b1633ae7b02602cdd4315919000a4eda6
Andreas Bergmo Johnsen <andreasbjohnsen@gmail.com>
2016-11-11 13:06:00 +0100

    Removed more uneeded code, added comments, changed picture alts etc.

--- main/scripts/gallery.js
+++ main/scripts/gallery.js
@@ -7,47 +7,27 @@
  Purpose: Javascript for "Bildegalleri".
  */
 
-function getElementLeft(funk) {
-    var x = 0;
-    x = funk.offsetLeft;
-    funk = funk.offsetParent;
-    while (funk !== null) {
-        x = parseInt(x) + parseInt(funk.offsetLeft);
-        funk = funk.offsetParent;
-    }
-    return x;
-}
 
-function getElementTop(funk) {
-    var y = 0;
-    y = funk.offsetTop;
-    funk = funk.offsetParent;
-    while (funk != null) {
-        y = parseInt(y) + parseInt(funk.offsetTop);
-        funk = funk.offsetParent;
-    }
-    return y;
-}
 
 function Large(obj) {
     var bigimage = document.getElementById("bigimage");
     var img = document.createElement("img");
     img.src = obj.src;
-    // img.style.maxWidth = '720px';
-    // img.style.maxHeight = '480px';
 
-    
+    // img.style.maxHeight = '480px' Decided to style this with the CSS (gallery.css) instead;
+    //max-width: 100% is also styled in CSS;
+
     bigimage.innerHTML = '';
     bigimage.appendChild(img);
-    //bigimage.style.left = (getElementLeft(obj) - 0) + 'px';
-    //bigimage.style.top = (getElementTop(obj) - 300) + 'px';
+    //bigimage.style.left = (getElementLeft(obj) - 0) + 'px'; (See CSS)
+    //bigimage.style.top = (getElementTop(obj) - 300) + 'px'; (See CSS)
 }
 
 function Out() {
     document.getElementById("bigimage").style.visibility = 'hidden';
 }
 
-// Set up default picture
+// Set up default picture ("0" is the first picture)
 console.log(document.getElementsByClassName("picelement")[0].childNodes[1]);
 Large(document.getElementsByClassName("picelement")[0].childNodes[1]);
 
